7dcb5c3300e96f989f2749f7a84b151afe906491
Adamor Henner <adacandido9@gmail.com>
2023-04-01 17:35:34 -0300

    Tratando erros

--- src/telas/DadosProduto/index.js
+++ src/telas/DadosProduto/index.js
@@ -4,18 +4,28 @@ import Botao from "../../componentes/Botao";
 import estilos from "./estilos";
 import React, { useState } from "react";
 import { salvarProduto } from "../../servicos/firestore";
+import { Alerta } from "../../componentes/Alerta";
 
 export default function DadosProduto({ navigation }) {
   const [nome, setNome] = useState('')
   const [preco, setPreco] = useState('')
-
+  const [mensagem, setMensagem] = useState('')
+  const [mostrarMensagem, setMostrarMensagem] = useState(false)
+  
   async function salvar(){
+    if(nome == '' || preco == ''){
+      setMensagem("Por favor preencha todos os campos")
+      setMostrarMensagem(true)
+      return
+    }
+
     const resultado = await  salvarProduto({
       nome,
       preco
     })
     if(resultado == 'erro'){
-      Alert.alert('Erro ao criar produto')
+      setMensagem("Erro ao salvar produto")
+      setMostrarMensagem(true)
     }
     else{
       navigation.goBack()
@@ -36,6 +46,12 @@ export default function DadosProduto({ navigation }) {
       />
 
       <Botao onPress={() => salvar()}>Salvar</Botao>
+
+      <Alerta
+        mensagem={mensagem}
+        error={mostrarMensagem}
+        setError={setMostrarMensagem}
+      />
 
     </View>
   )
